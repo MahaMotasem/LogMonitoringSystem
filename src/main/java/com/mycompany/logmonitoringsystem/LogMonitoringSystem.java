4c16c63e2f5c6a3085e61d58e0551c5887a964db
MahaMotasem <mahamotasem6@gmail.com>
2025-03-12 02:05:49 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/logmonitoringsystem/LogMonitoringSystem.java
@@ -0,0 +1,7 @@
+package com.mycompany.logmonitoringsystem;
+
+public class LogMonitoringSystem {
+    public static void main(String[] args) {
+        System.out.println("Log Monitoring System is running...");
+    }
+}
